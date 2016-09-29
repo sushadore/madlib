3550a6776fe40199cefa709078c88aea9d4374e3
jin camou and susha dore <susha@>
2016-09-28 19:10:29 -0700

    update page to implement an array and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,15 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    var blanks = ["location1", "person1", "person2", "question1", "verb1", "noun1"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    var location1Input = $("input#location1").val();
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var question1Input = $("input#question1").val();
-    var verb1Input = $("input#verb1").val();
-    var noun1Input = $("input#noun1").val();
 
-    $(".location1").text(location1Input);
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".question1").text(question1Input);
-    $(".verb1").text(verb1Input);
-    $(".noun1").text(noun1Input);
 
     $("#story").show();
 
     event.preventDefault();
-  });
+ });
 });
